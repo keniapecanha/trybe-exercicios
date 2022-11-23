913e37c5273f70343ee39a5aec68791646c5fc9e
keniapecanha <kenia.silvap@hotmail.com>
2022-11-23 15:43:46 -0300

    primeiro exercício de js

---
+++ fundamentos/4_introducao-a-javascript/1_javascript-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+const myname = 'Kênia';
+const birthCity = 'Coronel Fabriciano';
+let birthYear = 1986;
+
+console.log (myname, birthCity, birthYear);
+birthYear = 2030;
+console.log (birthYear);
+birthCity = 'Governador Valadares';
+console.log (birthCity);
