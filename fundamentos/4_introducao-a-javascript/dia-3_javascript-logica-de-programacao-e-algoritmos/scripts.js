64a6cb15a1f0104f81c3af94ca5f4b0d4f53a769
keniapecanha <kenia.silvap@hotmail.com>
2022-11-30 13:32:33 -0300

    Finalização dos exercícios

--- fundamentos/4_introducao-a-javascript/dia-3_javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/4_introducao-a-javascript/dia-3_javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,7 +1,5 @@
 // O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero
 // Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
-
-// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 let num = 10;
 let values = [];
 let fator = 1;
@@ -12,3 +10,13 @@ for (let index = num; index > 0; index-=1){
     fator = fator * values[index2];         
 }
 console.log(fator);
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+let word = 'tryber';
+let wordBackward;
+for (let index = word.length; index >= 0; index-=1){
+    wordBackward =  word[index];
+    console.log(wordBackward);
+} 
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
