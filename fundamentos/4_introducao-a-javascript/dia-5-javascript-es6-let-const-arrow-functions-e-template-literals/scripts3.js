41ff4e910d228d69211d54473c41eedaa7071a79
keniapecanha <kenia.silvap@hotmail.com>
2022-12-01 20:50:48 -0300

    finalizandos exercícios Parte 1

---
+++ fundamentos/4_introducao-a-javascript/dia-5-javascript-es6-let-const-arrow-functions-e-template-literals/scripts3.js
@@ -0,0 +1,5 @@
+// 🚀 Modifique a variável para que não ocorra Erro;
+// Copie o código abaixo e rode-o para verificar sua saída:
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
---
+++ fundamentos/4_introducao-a-javascript/dia-5-javascript-es6-let-const-arrow-functions-e-template-literals/scripts7.js
@@ -0,0 +1,10 @@
+// Transforme a função nomeCompleto em uma arrow function;
+// function nomeCompleto(nome, sobrenome) {
+//     return `${nome} ${sobrenome}`
+//   } 
+
+  const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
+
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome));
